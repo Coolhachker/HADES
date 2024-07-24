9eeb71e3724c8f63ae62c8e1ded3c57ebc400a54
_True_developer_ <74011549+Coolhachker@users.noreply.github.com>
2024-07-24 13:20:51 +0300

    Расписал векторизирование текста в файле vectorization_data.py. Решил использовать TextVectorization, вместо StringLookup

---
+++ src/Tools_to_set_the_dataset/vectorization_data.py
@@ -0,0 +1,34 @@
+from keras.layers import TextVectorization
+from tensorflow._api.v2.strings import lower, regex_replace
+from tensorflow import expand_dims
+import re
+import string
+
+
+class VectorizationData:
+    def __init__(self, path_to_file):
+        self.path_to_file = path_to_file
+        self.sequences_length = 350
+        self.vectorization_layer = self.set_vectorization_layer()
+        self.adapt_layer()
+
+    def chars_to_ids(self, text, label):
+        text = expand_dims(text, -1)
+        return self.vectorization_layer(text), label
+
+    @classmethod
+    def standardize(cls, text):
+        lower_case = lower(text)
+        return regex_replace(lower_case, "[%s]" % re.escape(string.punctuation))
+
+    def set_vectorization_layer(self):
+        return TextVectorization(
+            standardize=self.standardize,
+            output_mode="int",
+            max_tokens=self.sequences_length
+        )
+
+    def adapt_layer(self):
+        with open(self.path_to_file) as file:
+            for string_ in file:
+                self.vectorization_layer.adapt(string_)
