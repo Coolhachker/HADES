16b9cd48a454f84be7c89e7fbca7a097f0301a2b
_True_developer_ <74011549+Coolhachker@users.noreply.github.com>
2024-07-24 14:33:09 +0300

    написал датасет со спам сообщениями

--- src/Tools_to_set_the_dataset/set_dataset.py
+++ src/Tools_to_set_the_dataset/set_dataset.py
@@ -1,10 +1,41 @@
 from tensorflow.python.data import Dataset
 from src.Tools_to_set_the_dataset.vectorization_data import VectorizationData
+import logging
+from src.set_logger import ColoredFormat
+from tensorflow.python.data.experimental import AUTOTUNE
+file_handler = logging.FileHandler('../../logs/set_dataset.log', mode='w')
+file_handler.setFormatter(ColoredFormat())
+
+logging.getLogger().addHandler(file_handler)
+logging.getLogger().setLevel(logging.DEBUG)
+logger = logging.getLogger()
 
 
 class SpamDataset:
-    def __init__(self, path_to_file):
-        self.vectorization_layer = VectorizationData(path_to_file)
+    def __init__(self, path_to_file_with_spam_messages, path_to_file_with_simple_messages):
+        self.vectorization_layer = VectorizationData(path_to_file_with_spam_messages)
+        self.path_to_file_with_simple_messages = path_to_file_with_simple_messages
+        self.path_to_file_with_spam_messages = path_to_file_with_spam_messages
+        self.bath_size = 30
+        self.Dataset_from_spam_messages = self.set_spam_dataset()
+        self.set_label_to_spam(self.Dataset_from_spam_messages)
+        self.setting_spam_dataset()
 
     def set_spam_dataset(self):
-        pass
+        list_with_tensors: list = []
+        with open(self.path_to_file_with_spam_messages, 'r') as file:
+            for string in file:
+                list_with_tensors.append(self.vectorization_layer.chars_to_ids(string))
+                logging.debug(f'Размерность тензора: {self.vectorization_layer.chars_to_ids(string).shape} ')
+        return Dataset.from_tensor_slices(list_with_tensors)
+
+    def set_label_to_spam(self, dataset):
+        dataset = dataset.map(self.vectorization_layer.set_label_to_sequences_spam)
+
+    def setting_spam_dataset(self):
+        self.Dataset_from_spam_messages.cache().prefetch(AUTOTUNE)
+
+
+if __name__ == '__main__':
+    SpamDataset = SpamDataset('../../data/spam_messages.txt', '')
+    print(SpamDataset.Dataset_from_spam_messages)
--- src/Tools_to_set_the_dataset/vectorization_data.py
+++ src/Tools_to_set_the_dataset/vectorization_data.py
@@ -3,6 +3,8 @@ from tensorflow._api.v2.strings import lower, regex_replace
 from tensorflow import expand_dims
 import re
 import string
+from logging import getLogger
+logger = getLogger()
 
 
 class VectorizationData:
@@ -12,30 +14,35 @@ class VectorizationData:
         self.vectorization_layer = self.set_vectorization_layer()
         self.adapt_layer()
 
-    def chars_to_ids(self, text, label):
+    def chars_to_ids(self, text):
         text = expand_dims(text, -1)
-        return self.vectorization_layer(text), label
+        return self.vectorization_layer(text)
+
+    @classmethod
+    def set_label_to_sequences_spam(cls, tensor):
+        return tensor, 0
+
+    @classmethod
+    def set_label_to_sequences_simple(cls, tensor):
+        return tensor, 1
 
     @classmethod
     def standardize(cls, text):
         lower_case = lower(text)
-        return regex_replace(lower_case, "[%s]" % re.escape(string.punctuation))
+        return regex_replace(lower_case, "[%s]" % re.escape(string.punctuation), '')
 
     def set_vectorization_layer(self):
         return TextVectorization(
             standardize=self.standardize,
             output_mode="int",
-            max_tokens=self.sequences_length
+            max_tokens=100000,
+            output_sequence_length=self.sequences_length,
         )
 
     def adapt_layer(self):
         with open(self.path_to_file) as file:
-            for string_ in file:
-                self.vectorization_layer.adapt(string_)
+            self.vectorization_layer.adapt(file.readlines())
+        logger.info(f'Токены в слое TextVectorization: {self.vectorization_layer.get_vocabulary()}')
 
-    @property
-    def return_layer(self):
+    def __call__(self, *args, **kwargs):
         return self.vectorization_layer
-
-    def __new__(cls, *args, **kwargs):
-        return cls.return_layer
